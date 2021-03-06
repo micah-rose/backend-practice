5bc4b1c79f1d3c53bf54409244b0d7d04be182dd
Micah Lund <micah.r.lund@gmail.com>
2021-03-05 22:52:42 -0700

    Starter commit to get project in a repo.

---
+++ first_server/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const hostName = 'localhost';
+const port = 3000;
+
+const server  = http.createServer((req, res) => {
+    res.end('Welcome to Node!');
+});
+
+server.listen(port, hostName, () => {
+    console.log(`Server is running at ${hostName}:${port}`);
+});
