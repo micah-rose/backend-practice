fb3e499a01be78c66a3904d49df9a357070cf674
Micah Lund <micah.r.lund@gmail.com>
2021-03-07 18:44:56 -0700

    Assigned the request object to a url variable and prompted the response to return an end of 'translations' to indicate that our if condition logic worked as expected.

--- first_server/index.js
+++ first_server/index.js
@@ -4,7 +4,11 @@ const hostName = 'localhost';
 const port = 3000;
 
 const server  = http.createServer((req, res) => {
-    
+    const { url } = req;
+
+    if (url === '/translations') {
+        res.end('translations');
+    }
     res.end('Welcome to Node!');
 });
 
