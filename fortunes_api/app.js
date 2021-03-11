c43f011f00cad3cf283dbff298c2c4173337ca82
Micah Lund <micah.r.lund@gmail.com>
2021-03-11 15:56:34 -0700

    Created an executable file for our fortunes project.

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fortunes = require('./data/fortunes.json');
-const port = 3000;
 
 const app = express();
 
@@ -8,4 +7,4 @@ app.get('/fortunes', (req, res) => {
     res.json(fortunes);
 });
 
-app.listen(port), () => console.log(`Listening on port ${port}.`);
+module.exports = app;
