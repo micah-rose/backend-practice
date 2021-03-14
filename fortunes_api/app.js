72d61a7d87ff2134da89dce14f0ea22cf8cd009b
Micah Lund <micah.r.lund@gmail.com>
2021-03-13 18:58:46 -0700

    Started an API enpoint for pulling a random fortune by its ID.

--- fortunes_api/app.js
+++ fortunes_api/app.js
@@ -7,4 +7,8 @@ app.get('/fortunes', (req, res) => {
     res.json(fortunes);
 });
 
+app.get('/fortunes/random', (req, res) => {
+    //enpoint for pulling random fortunes by ID
+})
+
 module.exports = app;
