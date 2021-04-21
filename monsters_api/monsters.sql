da99854e274021accb6b4c9b691fb936bba3ab22
Micah Lund <micah.r.lund@gmail.com>
2021-04-21 15:02:52 -0600

    Set up the INSERT statements for inserting our data.

--- monsters_api/monsters.sql
+++ monsters_api/monsters.sql
@@ -15,3 +15,18 @@ CREATE TABLE lives(
     monster character varying(50),
     habitat character varying)50
 );
+
+INSERT INTO monsters(name, personality)
+VALUES ('Fluffy', 'aggressive'),
+('Noodles', 'impatient'),
+('Rusty', 'passionate');
+
+INSERT INTO habitats(name, climate, temperature)
+VALUES ('desert', 'dry', 100),
+('forest', 'haunted', 70),
+('mountain', 'icy', 30);
+
+INSERT INTO lives(monster, habitat)
+VALUES ('Fluffy', 'desert'),
+('Noodles', 'forest'),
+('Rusty', 'mountain');
