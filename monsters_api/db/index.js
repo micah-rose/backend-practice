15b4cf1f0b9ff25f74570980ee9279fff59560ff
Micah Lund <micah.r.lund@gmail.com>
2021-04-21 16:02:28 -0600

    Set up a query pool file to configure our database further.

---
+++ monsters_api/db/index.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'node_user',
+    host: 'localhost',
+    database: 'monstersdb',
+    password: 'node_password',
+    port: 5432
+})
+
+pool.query('SELECT * FROM monsters', (err, res) => {
+    if(err) return console.log(err);
+
+    console.log(res);
+})
