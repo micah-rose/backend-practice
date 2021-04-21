214813049c8262c0cd7dd5ca3c8e8db0f8b22094
Micah Lund <micah.r.lund@gmail.com>
2021-04-21 10:28:38 -0600

    Created a new folder for practicing use of PostgreSQL.

---
+++ psql/creatures.sql
@@ -0,0 +1,14 @@
+CREATE TABLE wizards (
+    name character varying(50),
+    power character varying(50)
+);
+
+CREATE TABLE elves(
+    name character varying(50),
+    speed real
+);
+
+CREATE TABLE hobbits(
+    name character varying(50),
+    personality character varying(50)
+);
