0448678ad20b59fc2ae6df8680c46d54ae29074a
Micah Lund <micah.r.lund@gmail.com>
2021-04-23 07:31:19 -0600

    Added error handling to our API.

--- monsters_api/app.js
+++ monsters_api/app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const pool = require('./db')
 const app = express();
 
-app.get('/monsters', (request, response) => {
+app.get('/monsters', (request, response, next) => {
     pool.query('SELECT * FROM monsters ORDER BY id ASC', (err, res) => {
-        if(err) return console.log(err);
-    
+        if(err) return next(err);
+ 
         response.json(res.rows);
     })
+})
+
+app.use((err, req, res, next) => {
+    res.json(err);
 })
 
 module.exports = app;
