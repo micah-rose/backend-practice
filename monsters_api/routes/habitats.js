0f7a08509179b565e0efeb7c90fe410b4fc7714b
Micah Lund <micah.r.lund@gmail.com>
2021-04-26 21:00:33 -0600

    Created a post request to add a new habitat.

--- monsters_api/routes/habitats.js
+++ monsters_api/routes/habitats.js
@@ -3,7 +3,7 @@ const pool = require('../db');
 
 const router = Router();
 
-router.get('/', () => {
+router.get('/', (request, response, next) => {
     pool.query(
     'SELECT * FROM habitats ORDER BY id ASC',
     (err, res) => {
@@ -11,6 +11,19 @@ router.get('/', () => {
 
         response.json(res.rows);
     })
+});
+
+router.post('/', (request, response, next) => {
+    const { name, climate, temperature } = request.body;
+
+    pool.query(
+        'INSERT INTO habitats(name, climate, temperature) VALUES ($1, $2, $3'),
+        [name, climate, temperature],
+        (err, res) => {
+            if(err) return next(err);
+
+            response.redirect('/habitats');
+        }
 })
 
 module.exports = router;
