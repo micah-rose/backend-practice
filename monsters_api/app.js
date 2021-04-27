6e83c2735966be8cece69915cd78d8ae89b328e8
Micah Lund <micah.r.lund@gmail.com>
2021-04-26 21:03:16 -0600

    Set up a route for the lives data.

--- monsters_api/app.js
+++ monsters_api/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const monsters = require('./routes/monsters')
 const habitats = require('./routes/habitats')
+const lives = require('./routes/lives')
 
 const app = express();
 
 app.use(express.json());
 app.use('/monsters', monsters);
 app.use('/habitats', habitats);
+app.use('/lives', lives);
 
 app.use((err, req, res, next) => {
     res.json(err);
